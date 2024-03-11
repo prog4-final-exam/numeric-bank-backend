8e6b06cdc8f8dd0d9503d7cb4f4aa13cbd94d699
fanambynana <hei.fanambinana@gmail.com>
2024-03-11 18:06:08 +0300

    initial commit

---
+++ src/test/java/my_bank/MyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package my_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
