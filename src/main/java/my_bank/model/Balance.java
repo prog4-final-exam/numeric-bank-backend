feb5cad48dacdcc6555b7fc8995a7cc4744666e6
fanambynana <hei.fanambinana@gmail.com>
2024-03-14 15:00:54 +0300

    feat: adding models
    feat: adding account model
    
    feat: adding balance model
    
    feat: adding withdrawal model
    
    feat: adding transfer model
    
    feat: adding transfer category model

---
+++ src/main/java/my_bank/model/Balance.java
@@ -0,0 +1,20 @@
+package my_bank.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Balance implements Serializable {
+    private int id;
+    private double mainBalance;
+    private double loanInterest;
+    private double loanAmount;
+    private LocalDateTime balanceDatetime;
+    private int idAccount;
+}
