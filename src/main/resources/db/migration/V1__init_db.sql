819b903cbd937bb48c971cddbd29216a29d72300
Harenabs21 <hei.harena.6@gmail.com>
2024-03-15 16:24:06 +0300

    fix: remove enum and columns useless in the sql file && upgrade mcd bank_v2

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS account
 (
     id                serial PRIMARY KEY,
-    first_name        VARCHAR(100)        NOT NULL,
-    last_name         VARCHAR(100)        NOT NULL,
-    birth_date        DATE CHECK (birth_date <= CURRENT_DATE - INTERVAL '21 YEAR'),
+    firstname        VARCHAR(100)        NOT NULL,
+    lastname         VARCHAR(100)        NOT NULL,
+    birthdate        DATE CHECK (birth_date <= CURRENT_DATE - INTERVAL '21 YEAR'),
     net_monthly_pay   DOUBLE PRECISION    NOT NULL,
     account_number    VARCHAR(100) UNIQUE NOT NULL,
     overdraft_allowed BOOLEAN DEFAULT false,
-    overdraft_limit   DOUBLE PRECISION
+    bank_name         VARCHAR(100) NOT NULL
 );
 
 
@@ -22,36 +22,17 @@ CREATE TABLE IF NOT EXISTS balance
     FOREIGN KEY (id_account) REFERENCES account (id)
 );
 
-CREATE TABLE withdrawal
+CREATE TABLE IF NOT EXISTS transaction
 (
     id              serial PRIMARY KEY,
     id_account      INT,
-    withdrawal_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    transaction_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     amount          DOUBLE PRECISION,
+    transaction_type VARCHAR(200) NOT NULL,
     FOREIGN KEY (id_account) REFERENCES account (id)
 );
 
--- create type enum for the credit and debit
-create type category_type as enum ('IN','OUT');
 
-CREATE TABLE IF NOT EXISTS transaction_category
-(
-    id   serial PRIMARY KEY,
-    name VARCHAR(100) not null,
-    type category_type
-);
-
-create table if not exists transaction_category_assignment
-(
-    id                      serial primary key,
-    id_transaction_category int,
-    comment                 text default null,
-    FOREIGN KEY (id_transaction_category) REFERENCES transaction_category (id)
-);
-
-
--- create type enum for transfer_type
-CREATE TYPE transfer_status AS ENUM ('Pending', 'Completed', 'Cancelled');
 
 CREATE TABLE IF NOT EXISTS transfer
 (
@@ -59,16 +40,24 @@ CREATE TABLE IF NOT EXISTS transfer
     id_account_source                  INT,
     id_account_destination             INT,
     id_transaction_category_assignment int,
-    transfer_date                      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    value_date                         TIMESTAMP,
+    transfer_datetime                      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    value_datetime                         TIMESTAMP,
     amount                             double precision not null,
     reason                             VARCHAR(200)     not null,
     label                              varchar(200),
-    status                             transfer_status,
-    is_external                        BOOLEAN          not null,
+    status                             VARCHAR(200),
     reference                          VARCHAR(200) unique not null,
     FOREIGN KEY (id_account_source) REFERENCES account (id),
-    FOREIGN KEY (id_account_destination) REFERENCES account (id),
-    FOREIGN KEY (id_transaction_category_assignment) REFERENCES transaction_category_assignment (id)
+    FOREIGN KEY (id_account_destination) REFERENCES account (id)
 );
 
+
+CREATE TABLE IF NOT EXISTS transfer_category
+(
+    id   serial PRIMARY KEY,
+    name VARCHAR(100) not null,
+    type VARCHAR(200) NOT NULL,
+    comment text default null,
+    id_transfer INT,
+    FOREIGN KEY (id_transfer) REFERENCES transfer (id)
+);
