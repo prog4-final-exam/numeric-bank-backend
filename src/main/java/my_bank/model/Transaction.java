ec83b66c25b3ff2275116826b51b8e8ba1e90d9c
Harenabs21 <hei.harena.6@gmail.com>
2024-03-15 16:38:50 +0300

    feat: create type enum TransactionType

---
+++ src/main/java/my_bank/model/Transaction.java
@@ -0,0 +1,20 @@
+package my_bank.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import my_bank.model.Enum.TransactionType;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Transaction implements Serializable {
+    private int id;
+    private double amount;
+    private LocalDateTime transactionDateTime;
+    private int idAccount;
+    private TransactionType transactionType;
+}
