9c3e16d67368452264a40aa0598cd3269da99086
fanambynana <hei.fanambinana@gmail.com>
2024-03-18 13:43:18 +0300

    feat: implement GET methods
    feat: implement GET /accounts & /accounts/{id}
    
    feat: implement GET /balances & /balances/{id}
    
    feat: implement GET /transactions & /transactions/{id}
    
    feat: implement GET /transfers/categories & /transfers/categories/{id}
    
    feat: implement GET /transfers & /transfers/{id}

--- src/main/java/my_bank/controller/AccountController.java
+++ src/main/java/my_bank/controller/AccountController.java
@@ -1,26 +1,40 @@
 package my_bank.controller;
 
+import lombok.AllArgsConstructor;
 import my_bank.model.Account;
-import my_bank.repository.AutoCrudOperation;
+import my_bank.service.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
+@AllArgsConstructor
 public class AccountController {
-    AutoCrudOperation<Account> accountAutoCrudOperation;
+    AccountService accountService;
+
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> findAll() {
+        return ResponseEntity.ok(accountService.findAll());
+    }
     @PostMapping("/accounts")
     public ResponseEntity<Account> save(@RequestBody Account toSave) {
-        return ResponseEntity.ok(accountAutoCrudOperation.save(toSave));
+        return ResponseEntity.ok(accountService.save(toSave));
     }
 
     @PutMapping("/accounts")
     public ResponseEntity<Account> update(@RequestBody Account toUpdate) {
-        return ResponseEntity.ok(accountAutoCrudOperation.update(toUpdate));
+        return ResponseEntity.ok(accountService.update(toUpdate));
     }
 
+
+    @GetMapping("/accounts/{id}")
+    public ResponseEntity<Account> findById(@PathVariable int id) {
+        return ResponseEntity.ok(accountService.findById(id));
+    }
     @DeleteMapping("/accounts/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable int id) {
-        return ResponseEntity.ok(accountAutoCrudOperation.deleteById(id));
+        return ResponseEntity.ok(accountService.deleteById(id));
     }
 }
--- src/main/java/my_bank/controller/TransferController.java
+++ src/main/java/my_bank/controller/TransferController.java
@@ -1,27 +1,39 @@
 package my_bank.controller;
 
+import lombok.AllArgsConstructor;
 import my_bank.model.Transfer;
 import my_bank.repository.AutoCrudOperation;
+import my_bank.service.TransferService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
+@AllArgsConstructor
 public class TransferController {
-    AutoCrudOperation<Transfer> transferAutoCrudOperation;
+    TransferService transferService;
 
+    @GetMapping("/transfers")
+    public ResponseEntity<List<Transfer>> findAll() {
+        return ResponseEntity.ok(transferService.findAll());
+    }
     @PostMapping("/transfers")
     public ResponseEntity<Transfer> save(@RequestBody Transfer toSave) {
-        return ResponseEntity.ok(transferAutoCrudOperation.save(toSave));
+        return ResponseEntity.ok(transferService.save(toSave));
     }
 
     @PutMapping("/transfers")
     public ResponseEntity<Transfer> update(@RequestBody Transfer toUpdate) {
-        return ResponseEntity.ok(transferAutoCrudOperation.update(toUpdate));
+        return ResponseEntity.ok(transferService.update(toUpdate));
     }
-
+    @GetMapping("/transfers/{id}")
+    public ResponseEntity<Transfer>  findById(@PathVariable int id) {
+        return ResponseEntity.ok(transferService.findById(id));
+    }
     @DeleteMapping("/transfers/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable int id) {
-        return ResponseEntity.ok(transferAutoCrudOperation.deleteById(id));
+        return ResponseEntity.ok(transferService.deleteById(id));
     }
 }
