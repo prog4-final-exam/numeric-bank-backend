1b80928c4d45d4fc0da3ed07fedf62e70df3bfd9
fanambynana <hei.fanambinana@gmail.com>
2024-03-18 11:31:22 +0300

    feat: findAll & findById

--- src/main/java/my_bank/repository/CrudOperation.java
+++ src/main/java/my_bank/repository/CrudOperation.java
@@ -1,7 +1,11 @@
 package my_bank.repository;
 
+import java.util.List;
+
 public interface CrudOperation<T> {
     T save(T toSave);
     boolean deleteById(int id);
     T update(T toUpdate);
+    List<T> findAll();
+    T findById(int id);
 }
