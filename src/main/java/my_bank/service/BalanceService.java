cc0fd7618fe0e0f928d2bd4b1835783613f92e18
fanambynana <hei.fanambinana@gmail.com>
2024-03-18 12:57:24 +0300

    feat: add service for all models
    feat: add Account service
    
    feat: add Balance service
    
    feat: add Transaction service
    
    feat: add Transfer service
    
    feat: add TransferCategory service

---
+++ src/main/java/my_bank/service/BalanceService.java
@@ -0,0 +1,28 @@
+package my_bank.service;
+
+import my_bank.model.Balance;
+import my_bank.repository.AutoCrudOperation;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BalanceService {
+    AutoCrudOperation<Balance> balanceAutoCrudOperation = new AutoCrudOperation<>(new Balance());
+
+    public List<Balance> findAll() {
+        return balanceAutoCrudOperation.findAll();
+    }
+    public Balance findById(int id) {
+        return balanceAutoCrudOperation.findById(id);
+    }
+    public Balance save(Balance toSave) {
+        return balanceAutoCrudOperation.save(toSave);
+    }
+    public Balance update(Balance toUpdate) {
+        return balanceAutoCrudOperation.update(toUpdate);
+    }
+    public boolean deleteById(int id) {
+        return balanceAutoCrudOperation.deleteById(id);
+    }
+}
