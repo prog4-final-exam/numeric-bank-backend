c625cb5178225d02b117344708fcf1f238c33a87
fanambynana <hei.fanambinana@gmail.com>
2024-03-19 17:02:25 +0300

    add generic model to represent all models

---
+++ src/main/java/my_bank/model/GenericModel.java
@@ -0,0 +1,14 @@
+package my_bank.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GenericModel {
+    Account account = new Account();
+    Balance balance = new Balance();
+    Transaction transaction = new Transaction();
+    Transfer transfer = new Transfer();
+    TransferCategory transferCategory = new TransferCategory();
+}
