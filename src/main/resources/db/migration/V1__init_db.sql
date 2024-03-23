02cacdc8c99fbf2bbe6633d6535a273cc4446411
Harenabs21 <hei.harena.6@gmail.com>
2024-03-23 21:39:40 +0300

    fix: replace the id_account_destination to destination_account_number

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS account
 (
     id                serial PRIMARY KEY,
-    firstname        VARCHAR(100)        NOT NULL,
-    lastname         VARCHAR(100)        NOT NULL,
-    birthdate        DATE CHECK (birthdate <= CURRENT_DATE - INTERVAL '21 YEAR'),
+    firstname         VARCHAR(100)        NOT NULL,
+    lastname          VARCHAR(100)        NOT NULL,
+    birthdate         DATE CHECK (birthdate <= CURRENT_DATE - INTERVAL '21 YEAR'),
     net_monthly_pay   DOUBLE PRECISION    NOT NULL,
     account_number    VARCHAR(100) UNIQUE NOT NULL,
     overdraft_allowed BOOLEAN DEFAULT false
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS balance
 (
     id            serial PRIMARY KEY,
     id_account    INT,
-    balance_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    balance_date  TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
     main_balance  DOUBLE PRECISION,
     loan_amount   DOUBLE PRECISION DEFAULT 0,
     loan_interest DOUBLE PRECISION DEFAULT 0,
@@ -23,11 +23,11 @@ CREATE TABLE IF NOT EXISTS balance
 
 CREATE TABLE IF NOT EXISTS transaction
 (
-    id              serial PRIMARY KEY,
-    id_account      INT,
+    id                   serial PRIMARY KEY,
+    id_account           INT,
     transaction_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    amount          DOUBLE PRECISION,
-    transaction_type VARCHAR(200) NOT NULL,
+    amount               DOUBLE PRECISION,
+    transaction_type     VARCHAR(200) NOT NULL,
     FOREIGN KEY (id_account) REFERENCES account (id)
 );
 
@@ -35,28 +35,27 @@ CREATE TABLE IF NOT EXISTS transaction
 
 CREATE TABLE IF NOT EXISTS transfer
 (
-    id                                 serial PRIMARY KEY,
-    id_account_source                  INT,
-    id_account_destination             INT,
-    transfer_datetime                      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    value_datetime                         TIMESTAMP,
-    amount                             double precision not null,
-    reason                             VARCHAR(200)     not null,
-    label                              varchar(200),
-    status                             VARCHAR(200),
-    reference                          VARCHAR(200) unique not null,
-    is_external_bank                   BOOLEAN  not null,
-    FOREIGN KEY (id_account_source) REFERENCES account (id),
-    FOREIGN KEY (id_account_destination) REFERENCES account (id)
+    id                         serial PRIMARY KEY,
+    id_account_source          INT,
+    destination_account_number VARCHAR(200) UNIQUE,
+    transfer_datetime          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    value_datetime             TIMESTAMP,
+    amount                     double precision    not null,
+    reason                     VARCHAR(200)        not null,
+    label                      varchar(200),
+    status                     VARCHAR(200),
+    reference                  VARCHAR(200) unique not null,
+    is_external_bank           BOOLEAN             not null,
+    FOREIGN KEY (id_account_source) REFERENCES account (id)
 );
 
 
 CREATE TABLE IF NOT EXISTS transfer_category
 (
-    id   serial PRIMARY KEY,
-    name VARCHAR(100) not null,
-    type VARCHAR(200) NOT NULL,
-    comment text default null,
+    id          serial PRIMARY KEY,
+    name        VARCHAR(100) not null,
+    type        VARCHAR(200) NOT NULL,
+    comment     text default null,
     id_transfer INT,
     FOREIGN KEY (id_transfer) REFERENCES transfer (id)
 );
