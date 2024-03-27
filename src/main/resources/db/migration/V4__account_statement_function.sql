76f6d4907206128fb3172b418f441ea7150a6aa8
Harenabs21 <hei.harena.6@gmail.com>
2024-03-27 15:38:29 +0300

    fix: rename the returned column

--- src/main/resources/db/migration/V4__account_statement_function.sql
+++ src/main/resources/db/migration/V4__account_statement_function.sql
@@ -1,32 +1,30 @@
-CREATE OR REPLACE FUNCTION account_statement(account_number_param INT)
-    RETURNS TABLE
-            (
-                account_number            INT,
-                operation_date            TIMESTAMP,
-                reference                 VARCHAR,
-                amount                    DOUBLE PRECISION,
-                balance_after_transaction DOUBLE PRECISION,
-                operation_motive          VARCHAR
-            )
-AS
+CREATE OR REPLACE FUNCTION account_statement(account_number_param 
+INT) RETURNS TABLE(date DATE, reference VARCHAR, reason 
+VARCHAR, credit_amount DOUBLE PRECISION, debit_amount DOUBLE 
+PRECISION, balance DOUBLE PRECISION) AS 
 $$
 BEGIN
-    RETURN QUERY
-        SELECT a.account_number,
-               t.transfer_datetime AS operation_date,
-               t.reference,
-               CASE
-                   WHEN t.id_account_source = a.id THEN -t.amount
-                   ELSE t.amount
-                   END             AS amount,
-               b.main_balance      AS balance_after_transaction,
-               t.reason            AS operation_motive
-        FROM account a
-                 JOIN
-             transfer t ON a.id = t.id_account_source OR a.id = t.id_account_destination
-                 JOIN
-             balance b ON a.id = b.id_account AND t.transfer_datetime = b.balance_date
-        WHERE a.id = account_number_param
-        ORDER BY t.transfer_datetime DESC;
+	RETURN QUERY
+	SELECT
+	    DATE(t.transfer_datetime) AS date,
+	    t.reference,
+	    t.reason,
+	    CASE
+	        WHEN t.id_account_source = a.id THEN NULL
+	        ELSE t.amount
+	    END AS credit_amount,
+	    CASE
+	        WHEN t.id_account_source = a.id THEN t.amount
+	        ELSE NULL
+	    END AS debit_amount,
+	    b.main_balance AS balance
+	FROM
+	    account a
+	    JOIN transfer t ON a.id = t.id_account_source
+	    JOIN balance b ON a.id = b.id_account
+	WHERE
+	    a.id = account_number_param
+	ORDER BY DATE(t.transfer_datetime) DESC;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql; 
